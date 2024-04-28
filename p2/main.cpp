c7d1a556779890fc73873fe1ad72810ae8072c19
Tzigu <andrei.tigaieriu@student.tuiasi.ro>
2024-04-29 00:27:30 +0300

    p2 -start

---
+++ p2/main.cpp
@@ -0,0 +1,19 @@
+#include "header.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+
+    
+
+
+
+
+
+
+    return 0;
+}
